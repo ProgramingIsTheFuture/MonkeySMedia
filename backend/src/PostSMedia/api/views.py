b652b63c665800ad34e5546796424d943128f1fa
Francisco Santos <franciscosantos@MacBook-Pro.lan>
2020-09-09 17:43:40 +0100

    Serializer and display all Posts

--- backend/src/PostSMedia/api/views.py
+++ backend/src/PostSMedia/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+
+from PostSMedia.models import Post
+from PostSMedia.serializers import PostSerializer
 
 @api_view(["GET"])
 def home(request):
-
-    return Response({"message": "Hello world!"}, status=200)
+    qs = Post.objects.all()
+    serializer = PostSerializer(qs, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
