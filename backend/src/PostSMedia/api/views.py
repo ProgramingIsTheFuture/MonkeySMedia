f499ead2a3b8251b679e899ad6cf7a87edc1f9f2
Francisco Santos <franciscosantos@MacBook-Pro.lan>
2020-09-10 00:06:41 +0100

    Last touch

--- backend/src/PostSMedia/api/views.py
+++ backend/src/PostSMedia/api/views.py
@@ -5,11 +5,13 @@ from rest_framework import status
 from PostSMedia.models import Post
 from PostSMedia.serializers import PostSerializer
 
+
 @api_view(["GET"])
 def list_posts_view(request):
     qs = Post.objects.all()
     serializer = PostSerializer(qs, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 @api_view(["POST"])
 def create_posts_view(request):
@@ -18,6 +20,5 @@ def create_posts_view(request):
         serializer.save(user=request.user)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    return Response(serializer.errors,
-        status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/src/PostSMedia/models.py
+++ backend/src/PostSMedia/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title= models.CharField(max_length=150, blank=True, null=True)
-    content = models.TextField(max_length=550, blank=True, null=True)
+    content = models.TextField(max_length=550, blank=True, null=False)
     image = models.FileField(upload_to='img', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
