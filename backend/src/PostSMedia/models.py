4ab0904f242103cf5c2b35ef47eef546834a6127
Francisco Santos <franciscosantos@MacBook-Pro.lan>
2020-09-09 17:36:14 +0100

    Creating the first Model

--- backend/src/PostSMedia/models.py
+++ backend/src/PostSMedia/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title= models.CharField(max_length=150, blank=True, null=True)
+    content = models.TextField(max_length=550, blank=True, null=True)
+    image = models.FileField(upload_to='img', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
