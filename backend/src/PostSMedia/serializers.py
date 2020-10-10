cc6bce70e12da004d9dad89151b1356ed2469eda
Francisco Santos <franciscosantos@MacBook-Pro.lan>
2020-10-10 15:16:36 +0100

    Adding Profile Image to Posts

--- backend/src/PostSMedia/serializers.py
+++ backend/src/PostSMedia/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework import serializers
 
 from .models import Post
-import inspect
+from ProfileSMedia.models import ProfileUser
+
 
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
-    timestamp = serializers.DateTimeField(format="%H:%M:%S - %d-%m-%Y", required=False)
+    timestamp = serializers.DateTimeField(
+        format="%H:%M:%S - %d-%m-%Y", required=False)
+    profile_image = serializers.SerializerMethodField()
     likes = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Post
-        fields = ('id', 'user', 'title', 'content', 'image', 'likes', 'timestamp')
+        fields = ('id', 'user', 'title', 'content', 'image',
+                  'profile_image', 'likes', 'timestamp')
 
     def get_user(self, obj):
         return obj.user.username
 
+    def get_profile_image(self, obj):
+        id = obj.user.id
+        image = ProfileUser.objects.get(id=id)
+        profile_image = image.profile_image
+        return '/media/' + str(profile_image)
+
     def get_likes(self, obj):
         return obj.likes.count()
-
