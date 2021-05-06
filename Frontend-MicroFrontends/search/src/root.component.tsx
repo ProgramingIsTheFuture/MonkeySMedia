90a048c79d0d18986fffb396ab973ec6e1ce1d08
Francisco Santos <franciscosantos@MacBook-Pro.Home>
2021-05-06 22:17:14 +0100

    Profile Align - Search Service

--- Frontend-MicroFrontends/search/src/root.component.tsx
+++ Frontend-MicroFrontends/search/src/root.component.tsx
@@ -87,22 +87,21 @@ export default function Root(props) {
               <PostSearch post={item} />
             </motion.div>
           ))}
-          <div>
-            {profileResponse.map((item: ProfileInfoTypes) => (
-              <motion.div
-                initial={{ scale: 0 }}
-                animate={{ rotate: 0, scale: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 260,
-                  damping: 20,
-                }}
-                key={item.id}
-              >
-                <ProfileSearch Profile={item} />
-              </motion.div>
-            ))}
-          </div>
+          {profileResponse.map((item: ProfileInfoTypes) => (
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ rotate: 0, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+              }}
+              style={{ width: "100%" }}
+              key={item.id}
+            >
+              <ProfileSearch Profile={item} />
+            </motion.div>
+          ))}
         </SearchedItems>
       </div>
     </Container>
