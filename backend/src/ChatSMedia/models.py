2ef061392a1a90d5c3af071a9b77dd93690635ac
Francisco Santos <franciscosantos@MacBook-Pro.Home>
2021-05-17 21:52:58 +0100

    Notification Model

--- backend/src/ChatSMedia/models.py
+++ backend/src/ChatSMedia/models.py
@@ -12,3 +12,17 @@ class Message(models.Model):
 
     class Meta:
         ordering = ["id"]
+
+
+class Notification(models.Model):
+    user = models.ForeignKey('auth.User', related_name="receiver", on_delete=models.CASCADE)
+    sender_username = models.TextField(blank=False, null=False)
+    message = models.TextField(blank=False, null=False)
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'Sender {self.sender_username}'
+
+    class Meta:
+        ordering = ["-id"]
